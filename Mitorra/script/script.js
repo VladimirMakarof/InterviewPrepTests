1a3c071691a2bb1b17578011edd5aaf711ff4494
VladimirMakarof <stan13.09@mail.ru>
2023-08-15 13:25:16 +0300

    add task 2

--- Mitorra/script/script.js
+++ Mitorra/script/script.js
@@ -1,10 +1,17 @@
 import { initCarousel } from './carousel.js';
 import { initJquery } from './jquery.min.js';
+import { sortBlocks } from './sortBlocks.js';
 
 document.addEventListener('DOMContentLoaded', function () {
+
+
   // Инициализация карусели
   initCarousel();
 
   // Инициализация jQuery-функций
   initJquery();
+
+
+  // Вызов функции для сортировки блоков
+  sortBlocks();
 });
